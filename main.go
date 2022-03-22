4377a595781143fd641bcc095edf9b7ef5753955
youssef elbeqqal <youssef.elbeqqal@skylark.ma>
2022-03-22 10:30:46 +0100

    Add .env file to the project.

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	// "log"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	// util "github.com/restuwahyu13/gin-rest-api/utils"
+	util "github.com/restuwahyu13/gin-rest-api/utils"
 )
 
 func getHelloWorld(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	c.IndentedJSON(http.StatusOK, gin.H{"say": "Hello, World!"})
 }
 
 func main() {
@@ -20,8 +20,7 @@ func main() {
 
 	router.GET("/api", getHelloWorld)
 
-	// log.Fatal(router.Run(":" + util.GodotEnv("GO_PORT")))
-	router.Run(":4444")
+	log.Fatal(router.Run(":" + util.GodotEnv("GO_PORT")))
 }
 
 func SetupRouter() *gin.Engine {
